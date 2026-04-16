253c0958e1c57aa2eb625285bbef4f8d1466c63e
Kenar Ben Shitrit <kenar529@email.com>
2026-04-16 17:46:22 +0300

    change api url

--- client/SmartSupport-Client/src/environments/environment.ts
+++ client/SmartSupport-Client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   googleClientId: '705675163391-t9hteaftk3cgqusn78859dgtltp39nm8.apps.googleusercontent.com',
-  API_URL:'https://smartsupport-rag-engine-production.up.railway.app'
+  // API_URL:'https://smartsupport-rag-engine-production.up.railway.app'
+  API_URL:'https://smartsupport-rag-engine-1.onrender.com'
 };
