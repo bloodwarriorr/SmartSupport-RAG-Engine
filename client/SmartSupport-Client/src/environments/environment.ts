a3c3b117c1bec42fb0f6bc17af72fdd29fa0e758
Kenar Ben Shitrit <kenar529@email.com>
2026-04-16 15:10:29 +0300

    Add Live server url

--- client/SmartSupport-Client/src/environments/environment.ts
+++ client/SmartSupport-Client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   googleClientId: '705675163391-t9hteaftk3cgqusn78859dgtltp39nm8.apps.googleusercontent.com',
-  API_URL:'http://localhost:7123'
+  API_URL:'https://smartsupport-rag-engine-production.up.railway.app'
 };
